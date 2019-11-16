bb35c86a5d19686f65667abe6384b187910c9930
a.antonov <a.antonov@pflb.ru>
2019-11-16 16:48:34 +0300

    named

--- src/test/java/to/deserialize/Model.java
+++ src/test/java/to/deserialize/Model.java
@@ -1,11 +1,12 @@
 package to.deserialize;
 
+import annotations.Hudson;
 import model.PInt;
 import model.PObject;
 import model.PString;
 
 public class Model {
-    
+    @Hudson("base")
     private String str="zoo";
     private Integer num=12;
     private String[] strs={"cow","horse","cat"};

7e2b94f3c4f32b14de47abdb1cc93241f85ed90e
a.antonov <a.antonov@pflb.ru>
2019-11-16 12:54:57 +0300

    PString

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,12 @@
 package main;
 
 import model.ISerializable;
+import model.PString;
 
 public class Main {
     static ISerializable model;
-    {
-
+    static {
+        model=new PString("name","value");
     }
 
     public static void main(String [] args){
--- src/main/java/model/PString.java
+++ src/main/java/model/PString.java
@@ -1,8 +1,15 @@
 package model;
 
 public class PString implements ISerializable {
+    private String name;
+    private String value;
+
+    public PString(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
 
     public String serialize() {
-        return null;
+        return String.format("\"%s\":\"%s\"",name,value);
     }
 }
